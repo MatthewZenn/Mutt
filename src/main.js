f2a3d140e29a5cf38af2ca3238b7fb7865d54577
Matthew Zenn <spyguy57@gmail.com>
2021-10-12 13:08:10 -0400

    Added analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueObserveVisibility from "vue-observe-visibility";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
 Vue.use(VueObserveVisibility)
+
+Vue.use(VueGtag, 
+  {  config: 
+    { id: "G-8CY9PZ3PLG" 
+  }});
 
 new Vue({
   render: h => h(App),
